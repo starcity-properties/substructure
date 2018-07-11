ea35c9ab3af50a3b2c220393989d97367869c0a0
therese <tdiede@gmail.com>
2018-07-11 08:31:31 -0700

    move iam stuff to separate tf file

--- datomic-transactor/main.tf
+++ datomic-transactor/main.tf
@@ -6,62 +6,6 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-
-  config {
-    bucket = "${var.tfstate_bucket}"
-    key    = "${var.vpc_remote_state_key}"
-    region = "${var.tfstate_region}"
-  }
-}
-
-# transactor role. ec2 instances can assume the role of a transactor
-resource "aws_iam_role" "transactor" {
-  name = "${var.transactor_name}-transactor"
-
-  assume_role_policy = <<EOF
-{
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Sid": ""
-    }
-  ],
-  "Version": "2012-10-17"
-}
-EOF
-}
-
-# policy with write access to cloudwatch
-resource "aws_iam_role_policy" "transactor_cloudwatch" {
-  name = "cloudwatch_access"
-  role = "${aws_iam_role.transactor.id}"
-
-  policy = <<EOF
-{
-  "Statement": [
-    {
-      "Action": [
-        "cloudwatch:PutMetricData",
-        "cloudwatch:PutMetricDataBatch"
-      ],
-      "Condition": {
-        "Bool": {
-          "aws:SecureTransport": "true"
-        }
-      },
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
 
 # s3 bucket for the transactor logs
 resource "aws_s3_bucket" "transactor_logs" {
@@ -73,57 +17,6 @@ resource "aws_s3_bucket" "transactor_logs" {
   }
 }
 
-# policy with write access to the transactor logs
-resource "aws_iam_role_policy" "transactor_logs" {
-  name = "s3_logs_access"
-  role = "${aws_iam_role.transactor.id}"
-
-  policy = <<EOF
-{
-  "Statement": [
-    {
-      "Action": [
-        "s3:PutObject"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "arn:aws:s3:::${aws_s3_bucket.transactor_logs.id}",
-        "arn:aws:s3:::${aws_s3_bucket.transactor_logs.id}/*"
-      ]
-    }
-  ]
-}
-EOF
-}
-
-data "aws_caller_identity" "current" {}
-
-# policy with complete access to the dynamodb table
-resource "aws_iam_role_policy" "transactor" {
-  name  = "dynamo_access"
-  role  = "${aws_iam_role.transactor.id}"
-  count = "${var.protocol == "ddb" ? 1 : 0}"
-
-  policy = <<EOF
-{
-  "Statement": [
-    {
-      "Action": [
-        "dynamodb:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "arn:aws:dynamodb:*:${data.aws_caller_identity.current.account_id}:table/${var.dynamodb_table}"
-    }
-  ]
-}
-EOF
-}
-
-# instance profile which assumes the transactor role
-resource "aws_iam_instance_profile" "transactor" {
-  name = "${var.transactor_name}-datomic-transactor"
-  role = "${aws_iam_role.transactor.name}"
-}
 
 # transactor launch config
 resource "aws_launch_configuration" "transactor" {

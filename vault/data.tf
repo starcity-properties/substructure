b38c1c69c67b02dd1c6476a6a7523dd9aa2ca8dc
therese <tdiede@gmail.com>
2018-07-26 14:44:41 -0700

    add route53 data

--- vault/data.tf
+++ vault/data.tf
@@ -7,3 +7,13 @@ data "terraform_remote_state" "vpc" {
     region = "${var.tfstate_region}"
   }
 }
+
+data "terraform_remote_state" "route53" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.tfstate_bucket}"
+    key    = "${var.route53_remote_state_key}"
+    region = "${var.tfstate_region}"
+  }
+}

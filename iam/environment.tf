fa921bcbf00e10c8e8065fb94aebb582887017c0
therese <tdiede@gmail.com>
2018-08-16 09:47:41 -0700

    add basic framework for roles and policies in environments

--- iam/environment.tf
+++ iam/environment.tf
@@ -1,25 +1,151 @@
-##-------------------------- ---------------------------##
+/*==== instance profile =======*/
+
+resource "aws_iam_instance_profile" "default" {
+  name  = "${var.access_type}-default_instance_profile"
+  roles = ["${aws_iam_role.default.name}"]
+}
+
+
+/*==== default role ===========*/
+
+resource "aws_iam_role" "default" {
+  name = "default_instance_profile"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "ec2.amazonaws.com",
+          "autoscaling.amazonaws.com",
+          "codedeploy.amazonaws.com"
+        ]
+      }
+    }
+  ],
+  "Version": "2012-10-17"
+}
+EOF
+}
+
+
+/*==== policy attachments =====*/
+
+resource "aws_iam_role_policy_attachment" "default" {
+  role       = "${aws_iam_role.default.name}"
+  count      = "${length(var.iam_policy_arns)}"
+  policy_arn = "${element(var.iam_policy_arns, count.index)}"
+}
 
 
 
+##------------------ ECS (APPLICATIONS) -------------------##
+
+/*==== service-linked roles ===*/
+
+resource "aws_iam_role" "ecs_service" {
+  name = "ecs_service"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecs_task_execution"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_autoscaling" {
+  name = "ecs_autoscaling"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "application-autoscaling.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
 
 
 
+##------------------ IAM (DEVELOPERS) ---------------------##
+
+resource "aws_iam_role" "iam_dev" {
+  name = "iam_developer"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "iam.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
 
 
 
+##------------------ IAM (ADMINISTRATORS) -----------------##
 
+resource "aws_iam_role" "iam_admin" {
+  name = "iam_administrator"
 
-
-
-
-
-
-
-
-
-
-
-
-
-##-------------------------- ---------------------------##
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "iam.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
--- iam/vars.tf
+++ iam/vars.tf
@@ -1,37 +1,50 @@
 # INHERITED
 
 variable "aws_region" {
-	description = "aws region"
+  description = "aws region"
 }
 
 
 
-# REQUIRED
+# REQUIRED GLOBAL
 
 variable "applications" {
-	type = "list"
-	default = []
+  type = "list"
+  default = []
 }
 
 variable "developers" {
-	type = "list"
-	default = []
+  type = "list"
+  default = []
 }
 
 variable "administrators" {
-	type = "list"
-	default = []
+  type = "list"
+  default = []
 }
 
 variable "dev_account" {
-	description = "number associated with aws development account (role)"
+  description = "number associated with aws development account (role)"
 }
 
 variable "stage_account" {
-	description = "number associated with aws staging account (role)"
+  description = "number associated with aws staging account (role)"
 }
 
 variable "prod_account" {
-	description = "number associated with aws production account (role)"
+  description = "number associated with aws production account (role)"
 }
 
+
+
+# ENVIRONMENT-SPECIFIC
+
+variable "iam_policy_arns" {
+  description = "list of policies to be attached to a role"
+  type = "list"
+  default = []
+}
+
+variable "db_access_type" {
+  description = "one of `app` (backend service) or `web` to express type of resource requiring access to database"
+}

32da5e853bf21274b94b43c90044a318db67ba89
therese <tdiede@gmail.com>
2018-08-20 18:43:12 -0700

    add env -> account-id vars; cleanup

--- iam/vars.tf
+++ iam/vars.tf
@@ -4,17 +4,24 @@ variable "aws_region" {
   description = "aws region"
 }
 
+variable "aws_account_ids" {
+  description = "aws account id's for all environments"
+  type = "map"
+}
 
-# REQUIRED ENVIRONMENT-SPECIFIC
+
+# CALCULATED
 
 variable "aws_account_id" {
   description = "the aws account id for respective environment: development, staging, or production"
+  default = "${lookup(var.aws_account_ids, var.environment, 'Not here!')}"
 }
 
-variable "iam_policy_arns" {
-  description = "list of policies to be attached to a role"
-  type = "list"
-  default = []
+
+# REQUIRED ENVIRONMENT-SPECIFIC
+
+variable "environment" {
+  description = "environment, or aws account"
 }
 
 variable "db_access_type" {
@@ -22,5 +29,10 @@ variable "db_access_type" {
 }
 
 variable "table_name" {
-  description = "the name of the DynamoDB table"
+  description = "the name of the database table"
+}
+
+variable "iam_policy_arns" {
+  description = "list of policies to be attached to a role"
+  type = "list"
 }

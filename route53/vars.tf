8809f33dc45e2f8edee0a187425586c8b1cec9d7
therese <tdiede@gmail.com>
2018-08-21 13:52:24 -0700

    initialize route53 substructure module

---
+++ route53/vars.tf
@@ -0,0 +1,29 @@
+# INHERITED
+
+variable "aws_region" {
+  description = "aws region"
+}
+
+variable "vpc_remote_state_key" {
+  description = "key of the vpc remote state within `tfstate_bucket`"
+}
+
+variable "tfstate_bucket" {
+  description = "bucket to find the remote terraform state"
+}
+
+variable "tfstate_region" {
+  description = "region of the remote terraform state bucket"
+}
+
+
+# ROUTE53
+
+variable "domain_name" {
+  description = "This is the name of the hosted zone."
+}
+
+variable "name_servers" {
+  description = "A list of name servers in associated (or default) delegation set."
+  type        = "list"
+}

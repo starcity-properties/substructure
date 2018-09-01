d32b5f563e9890f397b7c8b12cdd2df763e5fe4e
therese <tdiede@gmail.com>
2018-08-31 17:06:35 -0700

    attach policies to roles; finetune naming

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -13,3 +13,11 @@ output "ecs_task_role" {
 output "ecs_autoscaling_role" {
   value = "${aws_iam_role.ecs_autoscaling.arn}"
 }
+
+output "ecs_execution_role_id" {
+  value = "${aws_iam_role.ecs_execution.unique_id}"
+}
+
+output "ecs_task_policy" {
+  value = "${aws_iam_policy.ecs_task.arn}"
+}

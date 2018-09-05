5761871c72565ef5e5f9cb722144aa621e2c67b1
therese <tdiede@gmail.com>
2018-09-05 15:25:52 -0700

    use alias for A record to point to dns of elb

--- route53/main.tf
+++ route53/main.tf
@@ -34,22 +34,17 @@ resource "aws_route53_record" "ns" {
     "${aws_route53_zone.selected.name_servers.2}",
     "${aws_route53_zone.selected.name_servers.3}",
   ]
-
-  alias {
-    name                   = "${aws_elb.main.dns_name}"
-    zone_id                = "${aws_elb.main.zone_id}"
-    evaluate_target_health = true
-  }
 }
 
 resource "aws_route53_record" "www" {
   name    = "www.${aws_route53_zone.selected.name}"
   zone_id = "${aws_route53_zone.selected.zone_id}"
   type    = "A"
-  ttl     = "300"
 
-  records = [
-    "54.69.58.12",
-    "54.149.238.10"
-  ]
+  alias {
+    name    = "${data.terraform_remote_state.ecs.lb_dns_name}"
+    zone_id = "${data.terraform_remote_state.ecs.lb_zone_id}"
+
+    evaluate_target_health = true
+  }
 }

904a15edd95d56cafca4f32d71fc4858dd479103
therese <tdiede@gmail.com>
2018-09-07 13:56:27 -0700

    parameterize the properties script; cleanup vars across module

--- datomic-transactor/main.tf
+++ datomic-transactor/main.tf
@@ -33,27 +33,21 @@ resource "aws_s3_bucket" "transactor_logs" {
 }
 
 
-# instance profile which assumes the transactor role
-resource "aws_iam_instance_profile" "transactor" {
-  name = "${var.transactor_name}-dtx-transactor"
-  role = "${aws_iam_role.datomic_transactor.name}"
-}
-
-
 # transactor launch config
 resource "aws_launch_configuration" "datomic_transactor" {
   name_prefix          = "dtx-"
 
   image_id             = "${data.aws_ami.datomic_transactor.image_id}"
-  instance_type        = "${var.transactor_instance_type}"
+  instance_type        = "${var.instance_type}"
   iam_instance_profile = "${aws_iam_instance_profile.datomic_transactor.name}"
+  key_name             = "${var.ssh_key_name}"
 
-  # TODO:
-  security_groups = ["${data.terraform_remote_state.vpc.internal_inbound_id}"]
-  user_data       = "${data.template_file.transactor_user_data.rendered}"
-  key_name        = "${var.key_name}"
+  user_data = "${data.template_file.transactor_user_data.rendered}"
 
-  #  associate_public_ip_address = true
+  security_groups = [
+    "${data.terraform_remote_state.vpc.datomic_inbound_id}",
+    "${data.terraform_remote_state.vpc.http_outbound_id}"
+  ]
 
   ephemeral_block_device {
     device_name  = "/dev/sdb"
@@ -67,31 +61,38 @@ resource "aws_launch_configuration" "datomic_transactor" {
 
 
 data "template_file" "transactor_user_data" {
-  template = "${file("${path.module}/files/run-transactor.sh")}"
+  template = "${file("${path.module}/scripts/run-transactor.sh")}"
 
   vars {
     xmx                    = "${var.transactor_xmx}"
-    java_opts              = "${var.transactor_java_opts}"
-    region                 = "${var.aws_region}"
-    transactor_role        = "${aws_iam_role.datomic_transactor.name}"
+    xms                    = "${var.transactor_xms}"
+    java_opts              = "${var.java_opts}"
     memory_index_max       = "${var.transactor_memory_index_max}"
-    s3_log_bucket          = "${aws_s3_bucket.transactor_logs.id}"
     memory_index_threshold = "${var.transactor_memory_index_threshold}"
     object_cache_max       = "${var.transactor_object_cache_max}"
-    license_key            = "${var.license_key}"
-    cloudwatch_dimension   = "${var.cloudwatch_dimension}"
-    partition              = "${var.partition}"
-    protocol               = "${var.protocol}"
-    aws_dynamodb_table     = "${var.dynamodb_table}"
-    aws_dynamodb_region    = "${var.aws_region}"
+
+    region          = "${var.aws_region}"
+    transactor_role = "${aws_iam_role.datomic_transactor.name}"
+    peer_role       = "${data.terraform_remote_state.iam.datomic_peer_role}"
+
+    s3_log_bucket        = "${aws_s3_bucket.transactor_logs.id}"
+    cloudwatch_dimension = "${var.cloudwatch_dimension}"
+    dynamodb_table       = "${var.dynamodb_table}"
+    db_name              = "${var.db_name}"
+
+    partition   = "${var.partition}"
+    protocol    = "${var.protocol}"
+    port        = "${var.port}"
+    license_key = "${var.license_key}"
   }
 }
 
 
 # autoscaling group for launching transactors
 resource "aws_autoscaling_group" "datomic_asg" {
+  name = "${var.transactor_name}-dtx"
+
   availability_zones   = "${var.availability_zones}"
-  name                 = "${var.transactor_name}_transactors"
   max_size             = "${var.instance_count}"
   min_size             = "${var.instance_count}"
   launch_configuration = "${aws_launch_configuration.datomic_transactor.name}"

560380623f028037b590afe64436fad66bfd3001
therese <tdiede@gmail.com>
2018-09-07 14:01:20 -0700

    output the datomic uri

--- datomic-transactor/outputs.tf
+++ datomic-transactor/outputs.tf
@@ -1,3 +1,7 @@
 output "dynamodb_table_name" {
   value = "${aws_dynamodb_table.datomic.name}"
 }
+
+output "datomic_uri" {
+  value = "datomic:${var.protocol}://${var.aws_region}/${var.dynamodb_table}/${var.db_name}"
+}

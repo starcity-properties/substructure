f1a0878eea0a0efe28fc43394dc9f547ca6c5be0
therese <tdiede@gmail.com>
2018-10-11 13:14:37 -0700

    remove codedeploy module

--- codedeploy/vars.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region to create the S3 bucket in."
-}
-
-variable "name_prefix" {
-  description = "Prefix for the bucket name. Note that the same bucket is used for all codedeploy deployment groups"
-}

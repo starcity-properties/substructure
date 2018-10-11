9e11d62aab4a14bfb0f6d7e32f77f87884165038
therese <tdiede@gmail.com>
2018-10-11 13:10:24 -0700

    separate asg from logs

--- ecs/asg.tf
+++ ecs/asg.tf
@@ -1,37 +1,4 @@
-/*====
-Cloudwatch Log Group
-======*/
-
-
-resource "aws_cloudwatch_log_group" "app" {
-  name = "app_log_group"
-}
-
-
-/* metric used for auto scale */
-resource "aws_cloudwatch_metric_alarm" "service_cpu_high" {
-  alarm_name          = "${var.repository_name}-cpu_utilization_high"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/ECS"
-  period              = "60"
-  statistic           = "Maximum"
-  threshold           = "85"
-
-  dimensions {
-    ClusterName = "${aws_ecs_cluster.fargate.name}"
-    ServiceName = "${aws_ecs_service.service.name}"
-  }
-
-  alarm_actions = ["${aws_appautoscaling_policy.up.arn}"]
-  ok_actions    = ["${aws_appautoscaling_policy.down.arn}"]
-}
-
-
-/*====
-Auto Scaling for ECS
-======*/
+/*==== Auto Scaling for ECS ======*/
 
 resource "aws_appautoscaling_target" "target" {
   service_namespace  = "ecs"
